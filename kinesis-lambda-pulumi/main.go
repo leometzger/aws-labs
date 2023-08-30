f9c578beed54e629663fd88e3ede7105ec396baa
Leonardo Metzger <leonardo.metzger@outlook.com>
2023-08-30 08:54:54 -0300

    fix: fixes pulumi kinesis example

--- kinesis-lambda-pulumi/main.go
+++ kinesis-lambda-pulumi/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/kinesis"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/lambda"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -9,38 +8,18 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		function, err := createLambda(ctx)
+		if err != nil {
+			return err
+		}
+
 		stream, err := kinesis.NewStream(ctx, "lambda-integration-kinesis-stream", &kinesis.StreamArgs{
+			Name:            pulumi.String("labs-kinesis-lambda-stream"),
 			ShardCount:      pulumi.Int(1),
 			RetentionPeriod: pulumi.Int(24),
 			StreamModeDetails: kinesis.StreamStreamModeDetailsArgs{
 				StreamMode: pulumi.String("PROVISIONED"),
 			},
-		})
-		if err != nil {
-			return err
-		}
-
-		funcRole, err := iam.NewRole(ctx, "role-kinesis-consumer", &iam.RoleArgs{})
-		if err != nil {
-			return err
-		}
-		iam.NewPolicyAttachment(ctx, "lambda-policy", &iam.PolicyAttachmentArgs{
-			Name:      pulumi.String("rpa"),
-			Roles:     pulumi.All(funcRole),
-			PolicyArn: iam.ManagedPolicyAWSLambdaBasicExecutionRole,
-		})
-		iam.NewPolicyAttachment(ctx, "lambda-kinesis-attach", &iam.PolicyAttachmentArgs{
-			Name:      pulumi.String("lambda-kinesis-policy"),
-			Roles:     pulumi.All(funcRole),
-			PolicyArn: iam.ManagedPolicyAWSLambdaKinesisExecutionRole,
-		})
-
-		assets := make(map[string]interface{})
-		assets["."] = pulumi.NewFileArchive("./lambda")
-		function, err := lambda.NewFunction(ctx, "lambda-bla", &lambda.FunctionArgs{
-			Code:    pulumi.NewAssetArchive(assets),
-			Runtime: pulumi.String("go1.x"),
-			Role:    funcRole.Arn,
 		})
 		if err != nil {
 			return err
